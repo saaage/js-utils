2a75400f542100bdd02735d7fb58f9cfabd6c3df
Stefan Age <stefan.age@outlook.com>
2019-09-16 22:32:17 -0500

    added update, contains, ifAbsent...
    sleep, compareUsingKey

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,28 +1,63 @@
-export const isNil = arg => arg === null || arg === undefined
+import get from 'lodash.get'
 
-// takes an object and a key, returns the value or null
-export const get = (obj, str) => {
-  if (isNil(obj) || isNil(obj[str])) {
-    return null
-  } else return obj[str]
-}
+// is a thing null/undefined?
+export const isNil = arg => arg === null || arg === undefined
 
 // returns a new object containing only the keys selected
 export const selectKeys = (obj, keys) => {
   const map = {}
-  keys.forEach(k => map[k] = obj[k])
+  keys.forEach(k => (map[k] = obj[k]))
   return map
 }
 
-// returns the passed object with keys removed
+// returns a new object with keys removed
 export const removeKeys = (obj, keys) => {
-  keys.forEach(k => delete obj[k])
+  const clone = {...obj}
+  keys.forEach(k => delete clone[k])
+  return clone
+}
+
+// if arg is null or undefined, returns value
+export const ifAbsent = (arg, value) => {
+  if (isNil(arg)) {
+    return value
+  } else return arg
+}
+
+// mock some fn that takes a specific amount of time
+export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+// does the collection contain the value? (using f to derive comparison value as it iterates over collection)
+// if derive fn is not provided, checks collection for value itself
+export const contains = (coll, value, derive) => {
+  if (derive) {
+    let result = false
+    coll.forEach(i => {
+      if (derive(i) === value) result = true
+    })
+    return result
+  }
+
+  return coll.indexOf(value) > -1 ? true : false
+}
+
+
+// removes the first instance of value from the collection
+export const removeValue = (coll, val) => {
+  const result = Array.from(coll)
+  const index = result.indexOf(val)
+  if (index !== -1) result.splice(index, 1)
+  return result
+}
+
+// mutates obj[key] using fn and returns obj
+export const update = (obj, key, fn) => {
+  obj[key] = fn(obj[key])
   return obj
 }
 
-// if null or undefined is received, returns an empty string
-export const stringIfAbsent = arg => {
-  if (isNil(arg)) {
-    return ''
-  } else return arg
+export const compareUsingKey = (a, b, key) => {
+  if ((get(a, key)).toLowerCase() < get(b, key).toLowerCase()) return -1
+  if (get(a, key).toLowerCase() > get(b, key).toLowerCase()) return 1
+  return 0
 }
